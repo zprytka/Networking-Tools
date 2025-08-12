6360fe0c9aed13b686d6ab85c8ba96965831eec5
zprytka <51939032+zprytka@users.noreply.github.com>
2025-08-12 14:30:07 -0300

    Create netcat.py

---
+++ netcat.py
@@ -0,0 +1,22 @@
+import argparse
+import socket
+import shlex
+import subprocess
+import sys
+import textwrap
+import threading
+
+def execute(cmd):
+    cmd = cmd.strip()
+    if not cmd:
+        return
+    output = subprocess.check_output(shlex.split(cmd),stderr=subprocess.STDOUT)
+    return output.decode()
+
+if __name__ == 'main':
+    parser = argparse.ArgumentParser(
+        description='BHP Net Tool',
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=textwrap.dedent('''Example:
+            netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
+        ''')
